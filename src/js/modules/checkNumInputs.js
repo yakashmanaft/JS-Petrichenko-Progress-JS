393276a6965d9c2990487be8dfd1a5e312b8c450
Sergey <anfalov@camini-pr.ru>
2020-10-24 13:35:41 +0500

    Завершили тестовый проект

---
+++ src/js/modules/checkNumInputs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const checkNumInputs = (selector) => {
+    const numInputs = document.querySelectorAll(selector);
+
+    numInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+};
+
+export default checkNumInputs;
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,15 +1,12 @@
 'use strict';
 
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
     const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          phoneInputs = document.querySelectorAll('input[name = "user_phone"]');
-          
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+          inputs = document.querySelectorAll('input');
+         
+    checkNumInputs('input[name = "user_phone"]');
 
     const message = {
         loading: 'Загрузка',
@@ -42,6 +39,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === 'end'){
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
